7d41bfbc8a41c4bcdea739599be71d52fb9a5807
Jonathan McDonald <jonathanlmcd@gmail.com>
2019-10-03 15:29:17 -0600

    added 4th page, better color transitions

--- src/components/Page4.js
+++ src/components/Page4.js
@@ -2,9 +2,9 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 
 export default () => (
-  <Link to="/4">
-    <div className="page third">
-      <h1>Page 3</h1>
+  <Link to="/">
+    <div className="page fourth">
+      <h1>Page 4</h1>
     </div>
   </Link>
 )
